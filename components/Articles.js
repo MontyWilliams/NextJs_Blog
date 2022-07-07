49fca4cde47ffc3bc010890fd780bdebcd320832
Montywilliams <3650@holbertonschool.com>
2022-07-07 13:08:38 -0600

    create articleItem which takes the articles as props and sends them back in a styles component

--- components/Articles.js
+++ components/Articles.js
@@ -1,13 +1,11 @@
 import articleStyles from '../styles/article_styles.module.css';
+import ArticleItem from './ArticleItem.js'
 
 const Articles= ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
       {articles.map((article) => (
-       <div>
-        <h3>{article.title}</h3>
-        <img src={article.thumbnailUrl} alt="" />
-       </div>
+      <ArticleItem article={article}/>
       ))}
     </div>
   )
