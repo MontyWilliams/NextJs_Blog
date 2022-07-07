62a5ccdeab164fe1ba5f1604f909a293bb000e80
Montywilliams <3650@holbertonschool.com>
2022-07-07 17:08:14 -0600

    added meta component to article pages and set title to article title

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -2,18 +2,22 @@ import articleStyles from '../../../styles/article_styles.module.css';
 import {useRouter} from 'next/router'
 import Link from 'next/link'
 import {server} from '../../../config'
+import Meta from '../../../components/Meta'
 
 const article = ({article}) => {
   const router = useRouter()
   const {id} = router.query
 
   return (
-    <div className={articleStyles.card} >
-      <h1>{article.title}</h1>
-      <img src={article.thumbnailUrl} alt="" />
-      <br />
-      <Link href='/'>Goback</Link>
-    </div>
+    <>
+    <Meta title={article.title} />
+      <div className={articleStyles.card} >
+        <h1>{article.title}</h1>
+        <img src={article.thumbnailUrl} alt="" />
+        <br />
+        <Link href='/'>Goback</Link>
+      </div>
+    </>
   )
 }
 export const getStaticProps = async (context) => {
