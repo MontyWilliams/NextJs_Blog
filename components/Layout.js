826a88069fb557d9793c8ea0ab6d822d29205df7
Montywilliams <3650@holbertonschool.com>
2022-07-07 17:03:17 -0600

    created meta data component and placed it in layout, then adde meta component to about page so that I could customize the title

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import styles from "../styles/layout_styles.module.css"
 import Nav from "./Nav.js"
 import Header from './Header.js'
+import Meta from './Meta'
 
 const Layout = ({children}) => {
   return (
     <>
+    <Meta />
     <Nav />
       <div className={styles.container}>
         <main className={styles.main}>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import { server } from '../config'
-import Head from 'next/head'
+
 import Articles from '../components/Articles'
 
 export default function Home({articles}) {
   console.log(articles)
   return (
     <div>
-      <Head>
-        <title>Yea Yea</title>
-        <meta name='keywords' content='web development, programming'/>
-      </Head>
       <Articles articles={articles} />
     </div>
   )
